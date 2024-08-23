cc1f29e72d5993a94c954067ca7096aa8abd166c
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-23 20:11:41 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,9 @@
 import Contact from "../Contact/Contact";
 
-export default function ContactList({ contacts }) {
+export default function ContactList({ visibleListContacts }) {
   return (
     <ul>
-      {contacts.map((contact) => (
+      {visibleListContacts.map((contact) => (
         <li key={contact.id}>
           <Contact contact={contact} />
         </li>
