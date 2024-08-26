40f0e76896b2620cddcd69fbef7034f6ae745561
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-26 18:02:57 +0300

    Add id for input

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,21 @@
+import { useId } from "react";
+
 export default function SearchBox({ value, onHandleSetContactFilter }) {
+  const id = useId();
+
   const handleChange = (e) => {
     onHandleSetContactFilter(e.target.value);
   };
 
   return (
     <div>
-      <label></label>
-      <input type="text" value={value} onChange={handleChange} />
+      <label htmlFor={`${id}-search`}>Find contacts name by name</label>
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        id={`${id}-search`}
+      />
     </div>
   );
 }
