200e37da3feed77d6f835b5f2a18a841487b0311
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-27 13:19:22 +0300

    Add import "modern-normalize";

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 
+import "modern-normalize";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
