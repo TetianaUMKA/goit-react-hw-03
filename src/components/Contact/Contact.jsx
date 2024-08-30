21ba77c62685e8a816c7e6ac90c5396c9f7cf4eb
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-30 19:44:52 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,28 @@
+import { HiUser } from "react-icons/hi";
+import { HiPhone } from "react-icons/hi";
+
+import css from "./Contact.module.css";
+
 export default function Contact({
   contact: { id, name, number },
   onDeleteContact,
 }) {
   return (
     <>
-      <p>{name}</p>
-      <p>{number}</p>
+      <div>
+        <div className={css.contactData}>
+          <span>
+            <HiUser />
+          </span>
+          <p>{name}</p>
+        </div>
+        <div className={css.contactData}>
+          <span>
+            <HiPhone />
+          </span>
+          <p>{number}</p>
+        </div>
+      </div>
       <button type="button" onClick={() => onDeleteContact(id)}>
         Delete
       </button>
